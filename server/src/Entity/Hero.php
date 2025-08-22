ef9d6af6216a967e851153a7bd9d91807bbe93d8
ShockieToky <aymerick.guerinet@laposte.net>
2025-08-22 17:14:00 +0200

    Add initial setup for Hero entity, repository, and controller
    - Created Hero entity with an auto-generated ID.
    - Implemented HeroRepository for data access.
    - Added HeroController with a basic index route returning a JSON response.
    - Configured Doctrine migrations with a new YAML file.
    - Added .gitignore files for migrations and entity directories.
    - Generated Composer autoload files and updated installed packages.

---
+++ server/src/Entity/Hero.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\HeroRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: HeroRepository::class)]
+class Hero
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'a2e3b974365e5e4b98a9853c326c264c82668fa2',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'a2e3b974365e5e4b98a9853c326c264c82668fa2',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
